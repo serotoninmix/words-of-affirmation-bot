e593f2a8dcfc5bd6d45076f1f32c5698f102f9f9
serotoninmix <mrspectrumdesigns@gmail.com>
2024-07-09 21:50:24 +0100

    Update Google Sheets credentials path with alternative format

--- src/config.py
+++ src/config.py
@@ -2,5 +2,5 @@ import os
 
 TELEGRAM_TOKEN = os.getenv('TELEGRAM_TOKEN')
 CHAT_ID = os.getenv('CHAT_ID')
-GOOGLE_SHEETS_CREDENTIALS_PATH = 'src/edric-woa-bot-deb619a67c09.json'
+GOOGLE_SHEETS_CREDENTIALS_PATH = './src/edric-woa-bot-deb619a67c09.json'
 SHEET_NAME = 'Affirmations-WoA'
